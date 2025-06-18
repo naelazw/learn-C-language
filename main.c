8fa116461436d2080e8fbe0bf6a9dc941434a675
naelazw <naelazahwa487@gmail.com>
2025-06-18 09:46:46 +0700

    first commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // int age = 20;
+    // int years = 2025;
+    // int quantity = 2;
+
+    // printf("you are %d years old \n", age);
+    // printf("the year is %d \n", years);
+    // printf("u have ordered %d x items\n", quantity);
+
+    float gpa =2.5;
+    float price = 12.000;
+
+    printf("your gps is %.1f\n", gpa);
+    printf("the price is Rp%.3f\n", price);
+
+    return 0;
+}
